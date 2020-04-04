d79c9c5ba1328d7b1a0e96855564683e23a0a596
ElenaVan <vanyushkinelena@gmail.com>
2020-04-04 11:39:32 +0300

    added test OpenProFile

--- my-home-work-project/src/test/java/com/telran/qa/tests/OpenGoogleTest.java
+++ my-home-work-project/src/test/java/com/telran/qa/tests/OpenGoogleTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 
-import javax.tools.JavaCompiler;
 import java.util.concurrent.TimeUnit;
 
 public class OpenGoogleTest {
@@ -29,12 +28,24 @@ wd.findElement(By.className("gb_D")).click();
     @Test
     public void testSearchGoogle() throws InterruptedException {
 
-        wd.findElement(By.name("q")).click();
+        click();
         wd.findElement(By.name("q")).clear();
         wd.findElement(By.name("q")).sendKeys("java");
         Thread.sleep(3000);
     }
-        @AfterClass
+
+    private void click() {
+    }
+
+    private void click(By locator) {
+        locator.findElement(wd).click();
+    }
+    @Test
+    public void testOpenProFile()  throws InterruptedException{
+        click(By.id("gb_70"));
+        Thread.sleep(3000);
+    }
+    @AfterClass
     public void tearDown(){
 wd.quit();
     }
